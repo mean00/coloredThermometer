95c20e0757c899b86a647e180b1bd81c93898418
mean <fixounet@free.fr>
2019-10-09 07:55:17 +0200

    [temp] Temp reading

--- myCode.cpp
+++ myCode.cpp
@@ -2,7 +2,7 @@
 #include "Wire.h"
 #include "MapleFreeRTOS1000.h"
 #include "MapleFreeRTOS1000_pp.h"
-
+#include "DHT12.h"
 #include "simple7seg.h"
 
 void MainTask( void *a );
@@ -13,11 +13,12 @@ void myLoop() ;
 #define DSO_MAIN_TASK_PRIORITY 10
 
 simple7Seg *sevenSeg; 
-
+DHT12  *dht;
 void mySetup() 
 {
     
-    
+    dht=new DHT12();
+    dht->begin();
     afio_cfg_debug_ports( AFIO_DEBUG_SW_ONLY); // Unlock PB3 & PB4
 
     sevenSeg=simple7Seg::instantiate(PB12,PB13,PB14,PB15,     PA8,PA10,PB11,PB10, PA4,PA9,PB8,PA3);    
@@ -35,11 +36,12 @@ void MainTask( void *a )
 }
 void myLoop() 
 { 
-    
-    static float temp=12.34;
-    
+    //__STM32F1__
+    static float temp=98.59;
+    temp=dht->readTemperature();
+    temp-=2; // cheap calibration ....
     sevenSeg->printAsFloat((float)temp);
-    temp+=9.99;
+  //  temp+=9.99;
     
     xDelay(1000);    
 }
